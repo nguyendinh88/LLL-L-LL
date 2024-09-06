078a6a9da46aa71d1380dab5dea68a14cc0bb5bd
nguyendinh88 <niccinnicpsg@hotmail.com>
2024-09-06 14:27:28 +0800

    Add files via upload

---
+++ .electron-builder.config.js
@@ -0,0 +1,56 @@
+/**
+ * TODO: Rewrite this config to ESM
+ * But currently electron-builder doesn't support ESM configs
+ * @see https://github.com/develar/read-config-file/issues/10
+ */
+
+/**
+ * @type {() => import('electron-builder').Configuration}
+ * @see https://www.electron.build/configuration/configuration
+ */
+module.exports = async function () {
+  const {getVersion} = await import('./version/getVersion.mjs');
+  return {
+    productName: 'chrome-power',
+    directories: {
+      output: 'dist',
+      buildResources: 'buildResources',
+    },
+    files: ['packages/**/dist/**', 'packages/**/assets/**', 'migrations'],
+    extraResources: [
+      {
+        from: 'packages/main/src/native-addon/build/Release/',
+        to: 'app.asar.unpacked/node_modules/window-addon/',
+        filter: ['*.node'],
+      },
+      {
+        from: 'Chrome-bin.zip',
+        to: 'Chrome-bin.zip',
+      },
+      {
+        from: 'migrations',
+        to: 'app/migrations',
+      },
+      {
+        from: 'assets',
+        to: 'app/assets',
+      },
+    ],
+    extraMetadata: {
+      version: getVersion(),
+    },
+    asarUnpack: ['**/*.node'],
+    nsis: {
+      oneClick: false,
+      allowElevation: true,
+      allowToChangeInstallationDirectory: true,
+      createDesktopShortcut: true,
+      createStartMenuShortcut: true,
+      shortcutName: 'chrome-power',
+    },
+
+    win: {
+      requestedExecutionLevel: 'requireAdministrator',
+    },
+  };
+};
